d2936a5d02c32820c5a49c315306271055eb3cf9
Justin Tadlock <justintadlock@gmail.com>
2023-10-16 15:51:50 -0500

    Add initial files and folders.

---
+++ README.md
@@ -0,0 +1,3 @@
+# Custom Block Color Settings: Tutorial Plugin
+
+This repository is set up to showcase the code from the [Custom Block Color Settings](#) tutorial on the WordPress Developer Blog.
---
+++ plugin.php
@@ -0,0 +1,20 @@
+<?php
+/**
+ * Plugin Name:       Custom Block Color Settings
+ * Plugin URI:        https://github.com/...
+ * Description:       An example block with custom color settings.
+ * Version:           1.0.0
+ * Requires at least: 6.3
+ * Requires PHP:      7.4
+ * Author:            WordPress Developer Blog
+ * Author URI:        https://developer.wordpress.org/news/
+ * Text Domain:       custom-block-color-settings
+ */
+
+namespace DevBlog\CustomBlockColorSettings;
+
+add_action( 'plugins_loaded', __NAMESPACE__ . '\\setup' );
+
+function setup() {
+	register_block_type( untrailingslashit( __DIR__ ) . '/build' );
+}
